500dd6d97e2930187756e0eb063a55f0fc2a03b0
J.P. Krauss <jkrauss@asymworks.com>
2019-01-04 20:02:58 -0800

    Fixing check_config.sh

--- bin/check_config.sh
+++ bin/check_config.sh
@@ -3,7 +3,7 @@
 result=$(hass --config $TRAVIS_BUILD_DIR --script check_config)
 
 echo "$result"
-if [[ $result == *"ERROR"* ]] || [[ $result == *"error"* ]]
+if [[ $result == *"ERROR"* ]] || [[ $result == *"error"* ]] || [[ $result == *"Error"* ]]
 then
   exit 1
 fi
