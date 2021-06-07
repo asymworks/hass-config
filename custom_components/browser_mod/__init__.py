8f9894ff401c19d7e55cb022ae0a384f59d5003d
J.P. Krauss <jkrauss@asymworks.com>
2021-06-07 14:23:41 -0700

    Update custom_components for 2021.6

--- custom_components/browser_mod/__init__.py
+++ custom_components/browser_mod/__init__.py
@@ -1,5 +1,7 @@
 import logging
 
+from homeassistant import config_entries
+
 from .mod_view import setup_view
 from .connection import setup_connection
 from .service import setup_service
@@ -9,37 +11,65 @@ from .const import (
     DATA_ALIASES,
     DATA_ADDERS,
     CONFIG_DEVICES,
-    DATA_CONFIG
+    DATA_CONFIG,
+    DATA_SETUP_COMPLETE,
 )
+
+COMPONENTS = [
+    "media_player",
+    "sensor",
+    "binary_sensor",
+    "light",
+    "camera",
+]
 
 _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup(hass, config):
 
-    setup_view(hass)
+    if not hass.config_entries.async_entries(DOMAIN):
+        hass.async_create_task(
+            hass.config_entries.flow.async_init(
+                DOMAIN, context={"source": config_entries.SOURCE_IMPORT}, data={}
+            )
+        )
 
     aliases = {}
     for d in config[DOMAIN].get(CONFIG_DEVICES, {}):
         name = config[DOMAIN][CONFIG_DEVICES][d].get("name", None)
         if name:
-            aliases[name] = d.replace('_', '-')
+            aliases[name] = d.replace("_", "-")
 
     hass.data[DOMAIN] = {
         DATA_DEVICES: {},
         DATA_ALIASES: aliases,
         DATA_ADDERS: {},
         DATA_CONFIG: config[DOMAIN],
-        }
+        DATA_SETUP_COMPLETE: False,
+    }
 
-    async_load_platform = hass.helpers.discovery.async_load_platform
-    await async_load_platform("media_player", DOMAIN, {}, config)
-    await async_load_platform("sensor", DOMAIN, {}, config)
-    await async_load_platform("binary_sensor", DOMAIN, {}, config)
-    await async_load_platform("light", DOMAIN, {}, config)
-    await async_load_platform("camera", DOMAIN, {}, config)
+    await setup_connection(hass, config)
+    setup_view(hass)
+
+    for component in COMPONENTS:
+        hass.async_create_task(
+            hass.helpers.discovery.async_load_platform(component, DOMAIN, {}, config)
+        )
 
     await setup_service(hass)
-    await setup_connection(hass, config)
 
+    hass.data[DOMAIN][DATA_SETUP_COMPLETE] = True
+
+    for device in hass.data[DOMAIN][DATA_DEVICES].values():
+        device.trigger_update()
+
+    return True
+
+
+async def async_setup_entry(hass, config_entry):
+    for component in COMPONENTS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(config_entry, component)
+        )
     return True
